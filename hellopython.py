34f2eb91458b15ca7e3f296279a9fc11a9f4a46f
sathishkumar19 <sathishkumar19@gmail.com>
2018-04-21 13:39:07 +0200

    1st Python Project

---
+++ hellopython.py
@@ -0,0 +1,37 @@
+import random
+import sys
+import os
+
+
+
+print ("hello world")
+
+name ="Sathish"
+print (name)
+
+# This line is for comment
+'''
+This line is a multi line comment
+'''
+
+'''
+There are 5 different data types
+1.Numbers
+2.Strings
+3.lists
+4.Tuple
+5.Dictionary
+'''
+
+'''
+There are different Arithmetic operators
+1. + Addition
+2. - Subraction
+3. * Multiplication
+4. / Division
+5. % Modulus
+6. ** exponential calculation
+7. //
+'''
+
+# Example of Arithmetic  operations
